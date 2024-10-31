339ec671ddb8d7de7f706005faaf400e02b1c4ef
Sumair111 <jamalisameer247@gmail.com>
2024-10-31 17:29:15 +0500

    first commit

---
+++ Render a Welcome Message/script.js
@@ -0,0 +1,13 @@
+//Grab the welcome-el and store it in the variable WeclomeEl
+//Create two variable (name and greetings) that contain your name
+//and greeting we want to render on page
+//Render the welcome message using WelcomeEl.innertext
+
+
+let Name= "Sumair "
+let greetings= "Welcome Back"
+let WelcomeEl= document.getElementById("welcome-el")
+// let emoji=emojione.toshort("🐦")
+WelcomeEl.innerText= Name + greetings
+let sigma= "😎"
+WelcomeEl.innerText= Name + greetings + sigma
